6336199e414a709f2c3730fdd08c8325073cb881
Pinak17 <pinakroychowdhury@gmail.com>
2017-08-02 01:20:34 +0530

    HackerEarth SINISTER WRONG(to-do)August Easy Challenge

---
+++ sinister.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+long long unsigned int n;
+int main() {
+	long long unsigned int test;
+	cin>>test;
+	while(test--){
+		cin>>n;
+		sinister(n);
+		return 0;
+	}
+}
+
+void sinister(long long unsigned int n){
+	long long unsigned int a[n],i,q;
+	for(i=0;i<n;i++) {
+		cin>>a[i];
+	}
+	cin>>q;
+	while(q--) {
+		long long unsigned int c,a,b;
+		cin>>c;
+		if(c==1){
+			n++;
+			int b[n+1];
+			cin>>a;
+			
+			
+	}
+}
