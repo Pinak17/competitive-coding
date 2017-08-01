5e510510a432f04d7c81a4d03c976fc8486b6cbf
Pinak17 <pinakroychowdhury@gmail.com>
2017-08-02 01:21:48 +0530

    HackerRank APPLE ORANGES modified tutorial algorithm

--- tutorial_algo/appleorange.cpp
+++ tutorial_algo/appleorange.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 
 using namespace std;
@@ -38,44 +37,3 @@ int main(){
     cout<<co<<endl;
     return 0;
 }
-=======
-#include <iostream>
-
-using namespace std;
-
-int main(){
-    int s;
-    int t;
-    cin >> s >> t;
-    int a;
-    int b;
-    cin >> a >> b;
-    int m;
-    int n;
-    cin >> m >> n;
-    int ca=0,co=0;
-    vector<int> apple(m);
-    for(int apple_i = 0;apple_i < m;apple_i++){
-       cin >> apple[apple_i];
-    }
-    vector<int> orange(n);
-    for(int orange_i = 0;orange_i < n;orange_i++){
-       cin >> orange[orange_i];
-    }
-    for(int i=0;i<m;i++){
-        int d=a+apple[i];
-        if(d>=s && d<=t){
-            ca++;
-        }
-    }
-    for(int i=0;i<n;i++){
-        int d=b+orange[i];
-        if(d>=s && d<=t){
-            co++;
-        }
-    }
-    cout<<ca<<endl;
-    cout<<co<<endl;
-    return 0;
-}
->>>>>>> origin
