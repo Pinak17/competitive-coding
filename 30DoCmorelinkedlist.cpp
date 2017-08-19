4d1a5d38f8f9f8680ef0f08c0d71bddc4d09bc78
Pinak17 <pinakroychowdhury@gmail.com>
2017-08-20 00:02:17 +0530

    HackerRank MORE LINKED LIST (UPDATED)30 DAYS of CODE

--- 30DoCmorelinkedlist.cpp
+++ 30DoCmorelinkedlist.cpp
@@ -31,40 +31,33 @@ class Solution{
 			}
 				return head;
 		}
-		Node* insert(Node *head,int data){
-               Node* p=new Node(data);
-               if(head==NULL){
-                   head=p;  
+		Node* insert(Node *head,int data) {
+			Node* p=new Node(data);
+			if(head==NULL){
+				head=p;  
 
-               }
-               else if(head->next==NULL){
-                   head->next=p;
+			} else if(head->next==NULL){
+				head->next=p;
 
-               }
-               else{
-                   Node *start=head;
-                   while(start->next!=NULL){
-                       start=start->next;
-                   }
-                   start->next=p;   
-
+			} else{
+				Node *start=head;
+				while(start->next!=NULL){
+				   start=start->next;
 				}
-                    return head;
-                
-            
+				start->next=p;   
 			}
-			void display(Node *head){
-                  Node *start=head;
-                    while(start)
-                    {
-                        cout<<start->data<<" ";
-                        start=start->next;
-                    }
+				return head;           
+		}
+		void display(Node *head){
+			Node *start=head;
+			while(start) {
+				cout<<start->data<<" ";
+				start=start->next;
 			}
+		}
 };
 			
-int main()
-{
+int main() {
 	Node* head=NULL;
   	Solution mylist;
     int T,data;
@@ -74,7 +67,5 @@ int main()
         head=mylist.insert(head,data);
     }	
     head=mylist.removeDuplicates(head);
-
-	mylist.display(head);
-		
+	mylist.display(head);		
 }
